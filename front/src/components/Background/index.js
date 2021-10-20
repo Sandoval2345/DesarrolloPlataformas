11eaec628a1023695e47bef8eec8e9b8b42ee30a
ImLuu <lmc018@alumnos.ucn.cl>
2021-10-20 00:57:07 -0300

    intento de centrar texto con imagen
    unir ambas cosas

--- front/src/components/Background/index.js
+++ front/src/components/Background/index.js
@@ -1,15 +1,25 @@
 import React from 'react'
-import Body from '../Body'
+
+
 
 export default function Background() {
     return (
         <div>
             <div className="bg-gray-50">
-                <div class="opacity-30">
-                    <img src="https://dri.ucsc.cl/wp-content/uploads/sites/48/2019/08/Escudo-UCN-Logos.png" alt="..."/> 
-                </div>               
+                <div class="contenedor">
+                    <div class="opacity-30"> 
+                        <img src="https://dri.ucsc.cl/wp-content/uploads/sites/48/2019/08/Escudo-UCN-Logos.png" alt="..."/> 
+                    </div>    
+                    <div class="texto-sobre-imagen">
+                        Hola que tal
+                    </div>  
+                    <div class="centrado">
+                        Centrado
+                    </div>  
+                </div>
+                           
             </div>
-            <Body/>
+            
         </div>
     )
 }
--- front/src/page/Home/index.js
+++ front/src/page/Home/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Header from '../../components/Header'
-import Body from '../../components/Body'
+
 import Background from '../../components/Background'
 export default function Home() {
     return (
         <div >
          <Header/>
-         <Body/>
+         
          <Background/>
         </div>
     )
