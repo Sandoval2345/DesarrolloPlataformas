c31dda1dec06d78268b714ddccd0ad02501564ff
camiloalvarez98 <camiloalvarezduarte@gmail.com>
2021-10-29 14:01:11 -0300

    hh

--- front/src/pages/mallas/index.js
+++ front/src/pages/mallas/index.js
@@ -1,77 +1,56 @@
-//import './App.css';
-import React, {useState} from 'react';
-//import { ContenedorMallas } from '../../components'
-import { Contenedor } from '../../components'  
-import { Box, Button, Grid } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
-import DownloadForOfflineIcon from '@mui/icons-material/DownloadForOffline';
+import React from 'react'
+import MaterialTable from 'material-table';
+import { Contenedor } from '../../components'
+import { AlignHorizontalCenter } from '@mui/icons-material';
 
-const useStyles = makeStyles(theme=>({
-    root:{
-        backgroundColor: "white",
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'contain',
-        backgroundPosition: 'center center',
-        height: '100vh',
-        width: '300vh'
-        //justifyContent: 'center',
-        
-    },
-    text:{
-        textAlign: 'center',
-        marginTop: theme.spacing(5),
-       
-        color: 'black'
-    },
-    button:{
-        width: '5%',
-        margin: theme.spacing(5,155,10),
-        //marginTop: theme.spacing() ,
-        
-        [theme.breakpoints.down(400 + theme.spacing(2)+2)]:{
-            margin: theme.spacing(0),
-            width: '100%',
-            height: '100%'
-        }
-    },
+
+
+export default function Mallas() {
+
+    const columnas=[
+        {
+            title:'CARRERAS',
+            field:'carreras'
+        },
   
+    ];
+    const data=[
+        {carreras: 'Ingenieria Civil en Computacion e Informatica'},
+        {carreras: 'Ingenieria Civil Industrial'},
+        {carreras: 'Ingenieria en Tecnologias de Informacion'}
 
-}))
-
-export default function AppMallas(){
-    const classes = useStyles() 
+    ];
     return (
         <div>
             <Contenedor/>
-            <Grid container component = 'main' className = {classes.root}>
-                <h1 className = {classes.text} > Mallas Curriculares </h1>
-                <Button variant = 'contained'
-                        color = 'primary'
-                        size = 'large'
-                        className = {classes.button}
-                        endIcon = {<DownloadForOfflineIcon fontSize = 'large' />}
-                >
-                    Descargar
-                </Button>
-                <Button variant = 'contained'
-                        color = 'primary'
-                        size = 'large'
-                        className = {classes.button}
-                        endIcon = {<DownloadForOfflineIcon fontSize = 'large'/>}
-                >    
-                    
-                    Descargar
-                </Button>
-                <Button variant = 'contained'
-                        color = 'primary'
-                        size = 'large'
-                        className = {classes.button}
-                        endIcon = {<DownloadForOfflineIcon fontSize = 'large'/>}
-                >
-                    Descargar
-                </Button>
-            </Grid>
+            <MaterialTable
+                columns={columnas}
+                data = {data}
+                title = 'Mallas Academicas'
+                actions = {[
+                    {
+                        icon: 'check',
+                        tooltip: 'Malla Antigua',
+                    },
+                    {
+                        icon: 'check',
+                        tooltip: 'Malla Nueva'
+                    }
+                ]}
+                options={{
+                    actionsColumnIndex:20
+                }}
+                localization ={{
+                    header:{
+                        actions: 'Malla Antigua / Malla Nueva',
+                        
+                    }
+
+                }}
+            />
+             
+            
+
         </div>
     )
 }
-   
