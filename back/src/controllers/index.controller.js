3e21daeffbb933e42df093492ae2a3c812a64ead
camiloalvarez98 <camiloalvarezduarte@gmail.com>
2021-11-10 17:58:29 -0300

    Creacion tablas

--- back/src/controllers/index.controller.js
+++ back/src/controllers/index.controller.js
@@ -2,12 +2,16 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'postgres',
-    database: 'pruebabas',
+    host: 'ec2-34-224-239-147.compute-1.amazonaws.com',
+    user: 'qfoxsdbcxjlrwx',
+    password: '84ccd88118904fa7683b3c13b2e1a03fccbae0a4b867ed2861457fb815c5b4ec',
+    database: 'd5rkq2vcb1kc51',
     port: '5432'
 });
+
+const creacionTablas = async(req,res) =>{
+    const response = await pool.query('create table asignatura(ecin text primary key,departmento text,sede text,horas int,creditos int, nombre text); create table paralelo(corraltivo int primary key, nombre text, cuposMax int, inscritos int, ecin text references Asignatura(ecin), plataforma text, acta text, calificable text, disp text, rutProfesor references profesor(rut), titular text, semestre text); create table salas( aforomax int, codsala text primary key,tipo text);')
+}
 
 const getSalas = async(req, res) =>{
     const response = await pool.query('SELECT * FROM salas');
@@ -49,5 +53,6 @@ module.exports = {
     createSala,
     getSalaById,
     deleteSala,
-    updateSala
+    updateSala,
+    creacionTablas
 }
--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,11 +1,12 @@
 const { Router } = require('express')
 const router = Router()
 
-const { getSalas, createSala, getSalaById, deleteSala, updateSala } = require('../controllers/index.controller')
+const { getSalas, createSala, getSalaById, deleteSala, updateSala, creacionTablas } = require('../controllers/index.controller')
 router.get('/salas', getSalas)
 router.post('/salas',createSala) //ruta para guardar datos, por eso se usa post
 router.get('/salas/:codsala',getSalaById)
 router.delete('/salas/:codsala',deleteSala)
 router.put('/salas/:codsala',updateSala)
+router.post('/tablas', creacionTablas) //se crean tablas
 
 module.exports = router
