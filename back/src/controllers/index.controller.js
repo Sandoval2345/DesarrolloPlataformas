2ac8a90a0f6ad8affc672a37596964122667a780
camiloalvarez98 <camiloalvarezduarte@gmail.com>
2021-11-10 18:38:42 -0300

    avances

--- back/src/controllers/index.controller.js
+++ back/src/controllers/index.controller.js
@@ -1,6 +1,6 @@
 
 const { Pool } = require('pg');
-
+/**
 const pool = new Pool({
     host: 'ec2-34-224-239-147.compute-1.amazonaws.com',
     user: 'qfoxsdbcxjlrwx',
@@ -8,11 +8,27 @@ const pool = new Pool({
     database: 'd5rkq2vcb1kc51',
     port: '5432'
 });
+**/
+
+
+const pool = new Pool({
+    host: 'localhost',
+    user: 'postgres',
+    password: 'postgres',
+    database: 'db',
+    port: '5432'
+});
 
 const creacionTablas = async(req,res) =>{
-    const response = await pool.query('create table asignatura(ecin text primary key,departmento text,sede text,horas int,creditos int, nombre text); create table paralelo(corraltivo int primary key, nombre text, cuposMax int, inscritos int, ecin text references Asignatura(ecin), plataforma text, acta text, calificable text, disp text, rutProfesor references profesor(rut), titular text, semestre text); create table salas( aforomax int, codsala text primary key,tipo text);')
-    res.json('Tablas creadas')
+    const response = await pool.query('create table asignatura(ecin text primary key,departmento text,sede text,horas int,creditos int, nombre text); create table paralelo(corraltivo int primary key, nombre text, cuposMax int, inscritos int, ecin text references Asignatura(ecin), plataforma text, acta text, calificable text, disp text, rutProfesor references profesor(rut), titular text, semestre text); create table salas( aforomax int, codsala text primary key,tipo text);');
+    res.json('Tablas creadas');
 }
+
+const borrarTablas = async(req,res) =>{
+    const response = await pool.query('drop table asignatura; drop table profesor, drop table paralelo;');
+    res.json('Tablas borradas');
+}
+
 
 const getSalas = async(req, res) =>{
     const response = await pool.query('SELECT * FROM salas');
@@ -55,5 +71,6 @@ module.exports = {
     getSalaById,
     deleteSala,
     updateSala,
-    creacionTablas
+    creacionTablas,
+    borrarTablas
 }
