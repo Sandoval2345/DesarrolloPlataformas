635f7437b571050aa17b2f9e50eecf7737903eff
Sandoval2345 <tomas.sandoval@alumnos.ucn.cl>
2021-11-11 20:43:51 -0300

    Update salas.controller.js

--- back/src/controllers/salas.controller.js
+++ back/src/controllers/salas.controller.js
@@ -3,15 +3,15 @@ import Pool from "pg";
 
 let salasFunctions = {};
 
-const pool = new Pool({
+/*const pool = new Pool.Pool({
     host: 'ec2-34-224-239-147.compute-1.amazonaws.com',
     user: 'qfoxsdbcxjlrwx',
     password: '84ccd88118904fa7683b3c13b2e1a03fccbae0a4b867ed2861457fb815c5b4ec',
     database: 'd5rkq2vcb1kc51',
     port: '5432'
-});
+});*/
 
-/** 
+
 const pool = new Pool.Pool({
     host: 'localhost',
     user: 'postgres',
@@ -19,14 +19,14 @@ const pool = new Pool.Pool({
     database: 'pruebabas',
     port: '5432'
 });
-**/
 
-salasFuntions.creacionTablas = async(req,res) =>{
-    const response = await pool.query('create table asignatura(ecin text primary key,departamento text,sede text,horas int,creditos int, nombre text); create table paralelo(correlativo int primary key, nombre text, cuposMax int, inscritos int, ecin text,  foreign key (ecin) references Asignatura(ecin), plataforma text, acta text, calificable text, disp text, titular text, semestre text); create table salas( aforomax int, codsala text primary key,tipo text);');
+
+salasFunctions.creacionTablas = async(req,res) =>{
+    const response = await pool.query('create table asignatura(ecin text primary key,departamento text,sede text,horas int,creditos int, nombre text); create table paralelo(correlativo int primary key, nombre text, cuposMax int, inscritos int, ecin text,  foreign key (ecin) references Asignatura(ecin), plataforma text, acta text, calificable text, disp text, titular text, semestre text); create table salas( aforomax int, tipo text, codsala text primary key);');
     res.json('Tablas creadas');
 }
 
-salasFuntions.borrarTablas = async(req,res) =>{
+salasFunctions.borrarTablas = async(req,res) =>{
     const response = await pool.query('drop table asignatura; drop table profesor; drop table paralelo;');
     res.json('Tablas borradas');
 }
@@ -52,6 +52,7 @@ salasFunctions.createSala = async(req,res)=>{
         }
     })
 }
+
 
 salasFunctions.deleteSala = async(req, res)=>{ //se eliminara a traves de id por url
     const response = await pool.query('DELETE FROM salas WHERE codsala = $1',[req.params.codsala])
