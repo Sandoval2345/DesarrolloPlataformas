dcd48c03ab212ae315eb83f0921c186b38f8f669
scarlet-tobar <44346895+scarlet-tobar@users.noreply.github.com>
2021-11-15 12:07:40 -0300

    Conecta a heroku

--- back/src/controllers/salas.controller.js
+++ back/src/controllers/salas.controller.js
@@ -1,69 +1,82 @@
+import e from "express";
+import pool from "../Database/conexion.js"
 
-import Pool from "pg";
 
 let salasFunctions = {};
 
-const pool = new Pool.Pool({
-    host: 'ec2-34-224-239-147.compute-1.amazonaws.com',
-    user: 'qfoxsdbcxjlrwx',
-    password: '84ccd88118904fa7683b3c13b2e1a03fccbae0a4b867ed2861457fb815c5b4ec',
-    database: 'd5rkq2vcb1kc51',
-    port: '5432'
-});
 
-/*
-const pool = new Pool.Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'postgres',
-    database: 'pruebabas',
-    port: '5432'
-});
-*/
-
-
-salasFunctions.creacionTablas = async(req,res) =>{
-    const response = await pool.query('create table asignatura(ecin text primary key,departamento text,sede text,horas int,creditos int, nombre text); create table paralelo(correlativo int primary key, nombre text, cuposMax int, inscritos int, ecin text,  foreign key (ecin) references Asignatura(ecin), plataforma text, acta text, calificable text, disp text, titular text, semestre text); create table salas( aforomax int, tipo text, codsala text primary key);');
-    res.json('Tablas creadas');
+/**
+salasFunctions.consulta = async(req,res) =>{
+    res.setHeader("Content-type", "text/json");
+    console.log("holamundo");
+  
+    await pool
+      .query("select nombre, apellido from prueba")
+      .then((result) => {
+        console.log(result.rows);
+        res.send(result.rows);
+      })
+      .catch((e) => console.log(e));
+  
 }
+**/
 
-salasFunctions.borrarTablas = async(req,res) =>{
-    const response = await pool.query('drop table asignatura; drop table profesor; drop table paralelo;');
-    res.json('Tablas borradas');
-}
+
 
 salasFunctions.getSalas = async(req, res) =>{
-    const response = await pool.query('SELECT * FROM salas');
-    res.status(200).json(response.rows); //responde con un objeto json y el status es el codigo de estado, por defecto es 200
+    await pool
+        .query('SELECT aforomax, tipo, codsala FROM salas')
+        .then((result) => {
+        res.status(200).json(result.rows);
+        })
+        .catch((e) => console.log(e)); //responde con un objeto json y el status es el codigo de estado, por defecto es 200
 }
 
 salasFunctions.getSalaById = async(req, res) =>{ //el id se entrega por url
-    const response = await pool.query('SELECT * FROM salas WHERE codsala = $1',[req.params.codsala])//codigo en busqueda
-    res.json(response.rows)
+    await pool
+        .query('SELECT aforomax, tipo, codsala FROM salas WHERE codsala = $1',[req.params.codsala])//codigo en busqueda
+        .then((result) => {
+        res.status(200).json(result.rows);
+        })
+        .catch((e) => console.log(e));
+
 }
 
 salasFunctions.createSala = async(req,res)=>{
     const { aforoMax, tipo, codsala } = req.body; //datos de la nueva sala CREO que es un json
-    const response = await pool.query('INSERT INTO salas (aforoMax, tipo, codsala) VALUES ($1,$2,$3)',[aforoMax, tipo, codsala])
-    console.log(response);
-    res.json({
+    await pool
+    .query('INSERT INTO salas (aforoMax, tipo, codsala) VALUES ($1,$2,$3)',[aforoMax, tipo, codsala])
+    .then((result) => {
+        console.log(result);
+        res.json({
         message: 'Sala creada correctamente',
         body: {
             sala: {aforoMax, tipo, codsala}
         }
+        });
     })
+        .catch((e) => console.log(e));
 }
 
 
+
 salasFunctions.deleteSala = async(req, res)=>{ //se eliminara a traves de id por url
-    const response = await pool.query('DELETE FROM salas WHERE codsala = $1',[req.params.codsala])
-    res.json(`Sala ${req.params.codsala} eliminada correctamente`)
+    await pool
+    .query('DELETE FROM salas WHERE codsala = $1',[req.params.codsala])
+    .then((result) => {
+    res.json(`Sala ${req.params.codsala} eliminada correctamente`);
+    })
+    .catch((e) => console.log(e));
 }
 
 salasFunctions.updateSala = async(req, res)=>{
     const { aforoMax, tipo, codsala} = req.body; //datos posibles a edicion
-    const response = await pool.query('UPDATE salas SET aforoMax = $1, tipo = $2, codsala = $3 WHERE codsala = $4',[aforoMax,tipo,codsala, req.params.codsala])
-    res.json('sala actualizada correctamente')
+    await pool
+    .query('UPDATE salas SET aforoMax = $1, tipo = $2, codsala = $3 WHERE codsala = $4',[aforoMax,tipo,codsala, req.params.codsala])
+    .then((result) => {
+        res.json('sala actualizada correctamente');
+    })
+    .catch((e) => console.log(e));
 }
 
 export default salasFunctions;
