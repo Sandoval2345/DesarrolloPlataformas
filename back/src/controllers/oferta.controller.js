8cdb9a58149185a27c0688a2fcdceb9910b895b0
scarlet-tobar <44346895+scarlet-tobar@users.noreply.github.com>
2021-12-08 22:06:59 -0300

    Entrega de oferta y guargado de oherta en 1 json

--- back/src/controllers/oferta.controller.js
+++ back/src/controllers/oferta.controller.js
@@ -3,35 +3,53 @@ import pool from "../Database/conexion.js"
 let ofertaFunctions= {};
 
 ofertaFunctions.estimacionPrereqAsignatura = async(req,res) =>{
-    const {ecin, departamento, semestre, promedioReprobadosP, promedioRenunciasP, promedioReprobados, promedioRenuncias, aforoSala} = req.body;
-    console.log(req.body)
+    const {semestre, promedioReprobadosP, promedioRenunciasP, promedioReprobados, promedioRenuncias, aforoSala} = req.body;
+
     res.setHeader("Content-type", "text/json");
     await pool
-      .query("select sum(inscritos) from paralelo where ecin= $1 and materia=$2 and semestre < $3 limit 4; ",[ecin, departamento, semestre])
+      .query("select ecin, materia, sum(inscritos)/count(inscritos) as suma from paralelo where semestre < $1 group by ecin,materia ; ",[semestre])
       .then((result) => {
-    
-    const inscritos =result.rows[0].sum
-    console.log(inscritos)
-     let estReq= inscritos/4 -promedioRenunciasP-promedioReprobadosP;
+    let j={};
+    j.semestre=semestre;
+    for (var i=0; i<result.rowCount; i++) {
+     let inscritos =result.rows[i].suma
+     let estReq= inscritos -promedioRenunciasP-promedioReprobadosP;
      let demEst= Math.ceil(estReq+promedioReprobados+promedioRenuncias);
      let paralelos= Math.ceil(demEst/aforoSala);
-     console.log(estReq,paralelos)
-     res.status(200).send({demEst,paralelos});
+     let deAsignatura={};
+     deAsignatura.ecin=result.rows[i].ecin;
+     deAsignatura.materia=result.rows[i].materia;
+     deAsignatura.demEst=demEst;
+     deAsignatura.paralelos=paralelos;
+     j[i]=deAsignatura;
+    }
+    console.log(j)
+    res.status(200).send(JSON.parse(JSON.stringify(j)));
     })
       .catch((e) => console.log(e));
       
 }
 
 ofertaFunctions.guardarOfertaAsignatura = async(req,res) =>{
-    const {ecin, departamento,semestre, demEstima, paralelos} = req.body;
+  console.log(req.body)
+  const re=req.body;
+  var semestre=re.semestre;
+  for (var i=0;i<199;i++){
+    var ecin=re[i].ecin;
+    var cantParalelos=re[i].paralelos;
+    var demEstima=re[i].demEstima;
+    var departamento=re[i].materia
     res.setHeader("Content-type", "text/json");
+    await pool .query("insert into ofertaAcademica values ($1) ON CONFLICT DO NOTHING;;",[semestre]).catch((e) =>console.log(e));
+
     await pool
-      .query("insert into ofertaAcademica values ($1); insert into ofertaAsignatura(semestre, ecin, departamento, cantParalelos, demandaEstimada) values ($2,$3,$4,$5,$6);",[semestre,semestre, ecin, departamento,demEstima, paralelos])
+      .query(" insert  into ofertaAsignatura(semestre, ecin, departamento, cantParalelos, demandaEstimada) values ($1,$2,$3,$4,$5) ON CONFLICT (semestre, ecin, departamento) do update set cantparalelos=$6, demandaEstimada=$7;",[semestre, ecin, departamento,demEstima, cantParalelos,demEstima, cantParalelos])
       .then((result) => {
+    
      res.json("Oferta Ingresada")
     })
       .catch((e) => console.log(e));
       
-}
+}}
 export default ofertaFunctions;
 
